e85c11f71ea45e6caf2138e0bb9b5d575116279a
JaceMonforton <122835284+JaceMonforton@users.noreply.github.com>
2024-03-28 14:56:17 -0400

    Updates

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import { setUser } from '../redux/userSlice';
 import {showLoading , hideLoading} from '../redux/alertSlice';
